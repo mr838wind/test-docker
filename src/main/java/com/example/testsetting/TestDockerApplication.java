8ce4b2f399581a7c4408bc25dc974dcb2da847cb
chhan <chhan@ebrainsoft.com>
2022-08-05 18:22:23 +0900

    [feature] war packaging: todo debug

--- src/main/java/com/example/testsetting/TestDockerApplication.java
+++ src/main/java/com/example/testsetting/TestDockerApplication.java
@@ -3,10 +3,12 @@ package com.example.testsetting;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @Slf4j
-public class TestDockerApplication {
+//public class TestDockerApplication {
+public class TestDockerApplication extends SpringBootServletInitializer { //[war packaging]
 
     public static void main(String[] args) {
         SpringApplication.run(TestDockerApplication.class, args);
